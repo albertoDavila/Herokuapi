617c92ccc051d7c3bd66cbb61f3a580e04c3a9bd
albertoDavila <48994619+albertoDavila@users.noreply.github.com>
2020-02-06 15:05:52 -0600

    Update server.js

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.use( morgan( "dev" ) );
 // 		});
 // });
 
-app.get( "/api/gastosVP", ( req, res, next ) => {
+app.get( "/api/RegistrosE", ( req, res, next ) => {
 	VPList.get()
 		.then( vicepresidencia => {
 			return res.status( 200 ).json( vicepresidencia );
