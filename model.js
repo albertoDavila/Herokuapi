9dba9800248d8e672cd896c7605e666c1a088c28
albertoDavila <48994619+albertoDavila@users.noreply.github.com>
2020-02-27 15:17:45 -0600

    Update model.js

--- model.js
+++ model.js
@@ -11,7 +11,7 @@ let vicepresidenciaSchema = mongoose.Schema({
 	pregunta3: { type: String },
 	pregunta4: { type: String },
 	pregunta5: { type: String },
-	promedio: { type: Integer },
+	promedio: { type: Number },
 	podcast: { type: String },
 	audiolibro: { type: String },
 	libro: { type: String }
